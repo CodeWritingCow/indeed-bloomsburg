1458ff089538c39dae901c40ef2118164c03eea9
Gary Pang <gary.pang@gmail.com>
2016-06-11 02:27:23 -0400

    Add routes
    Build skeleton for app routes. TO DO: Add code to each route so it can
    access Indeed.com's API.

--- server.js
+++ server.js
@@ -2,9 +2,14 @@
 // =======================================
 
 // LOAD NPM PACKAGES -------------------------
+require('dotenv').config(); // dotenv loads environmental variables. It works in development environment only!
+
 var express = require('express'), // express is a 'fast, unopinionated minimalist web framework'
 	app = express(), // initialize app with express
-	morgan = require('morgan'); // morgan is a HTTP request logger middleware
+	morgan = require('morgan'), // morgan is a HTTP request logger middleware
+	request = require('request'), // request makes HTTP calls
+	api = require('indeed-api').getInstance(process.env.PUBLISHER_ID); // package that accesses Indeed.com's API. Our access token is saved inside an environmental variable.
+
 
 // log all HTTP requests in the console
 app.use(morgan('dev'));
@@ -12,11 +17,34 @@ app.use(morgan('dev'));
 
 // SET THE ROUTES
 // =======================================
+// "req, res" stand for "request, response."
 
-// home page route
-app.get('/', function(request, response) {
-	response.send('This is the home page.');
+// home page
+app.get('/', function(req, res) {
+	res.send('This is the home page.');
 });
+
+// berwick jobs
+app.get('/berwick', function(req, res) {
+	res.send('List Berwick jobs here!');
+});
+
+// bloomsburg jobs
+app.get('/bloomsburg', function(req, res) {
+	res.send('List Bloomsburg jobs here!');
+});
+
+// danville jobs
+app.get('/danville', function(req, res) {
+	// body...
+	res.send('Danville jobs!');
+});
+
+// geisinger jobs
+app.get('/geisinger', function(req, res) {
+	res.send('Get a Geisinger job!');
+});
+
 
 // START THE SERVER
 // =======================================
