0195346ab7b915b361efb6d6542bc2cdab315a0b
Gary Pang <gary.pang@gmail.com>
2016-06-17 02:15:09 -0400

    Update route
    Geisinger route displays up to 20 job search results. Edit result.ejs so
    it displays different town name according to job search.

--- server.js
+++ server.js
@@ -51,7 +51,6 @@ app.get('/test', function(req, res) {
 	});
 });
 
-
 // berwick jobs 
 app.get('/berwick', function(req, res) {
 	request('http://api.indeed.com/ads/apisearch?publisher=' + process.env.PUBLISHER_ID + '&format=json' + '&l=berwick%2C+pa&sort=&radius=&st=&jt=&start=&limit=20&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
@@ -118,22 +117,42 @@ app.get('/danville', function(req, res) {
 // Search query with parameters.
 // ex. '/test/wilkes-barre/pa'
 app.get('/test/:city/:state', function(req, res) {	
-	request('http://api.indeed.com/ads/apisearch?publisher=' + process.env.PUBLISHER_ID + '&format=json' + '&l=' + req.params.city + '%2C+' + req.params.state + '&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
+	request('http://api.indeed.com/ads/apisearch?publisher=' + process.env.PUBLISHER_ID + '&format=json' + '&l=' + req.params.city + '%2C+' + req.params.state + '&sort=&radius=&st=&jt=&start=&limit=20&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
 		if (!error && response.statusCode == 200) {
-			console.log(body);
-			res.write(body);
-			res.end();
+			var data = JSON.parse(body);
+			res.render('pages/results', {
+				searchTotalResults: data.totalResults,
+				searchLocation: data.location,
+				searchJobTitle: data.results[0].jobtitle,
+				searchCompany: data.results[0].company,
+				searchDate: data.results[0].date,
+				searchSnippet: data.results[0].snippet,
+				searchUrl: data.results[0].url,
+				searchFormattedLocationFull: data.results[0].formattedLocationFull,
+				searchRelativeTime: data.results[0].formattedRelativeTime,
+				searchResults: data.results
+			});
 		}
 	});
 });
 
 // geisinger jobs
 app.get('/geisinger', function(req, res) {
-	request('http://api.indeed.com/ads/apisearch?publisher=' + process.env.PUBLISHER_ID + '&format=json' + '&q=geisinger&l=bloomsburg%2C+pa&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
+	request('http://api.indeed.com/ads/apisearch?publisher=' + process.env.PUBLISHER_ID + '&format=json' + '&q=geisinger&l=bloomsburg%2C+pa&sort=&radius=&st=&jt=&start=&limit=20&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
 		if (!error && response.statusCode == 200) {
-			console.log(body);
-			res.write(body);
-			res.end();
+			var data = JSON.parse(body);
+			res.render('pages/results', {
+				searchTotalResults: data.totalResults,
+				searchLocation: data.location,
+				searchJobTitle: data.results[0].jobtitle,
+				searchCompany: data.results[0].company,
+				searchDate: data.results[0].date,
+				searchSnippet: data.results[0].snippet,
+				searchUrl: data.results[0].url,
+				searchFormattedLocationFull: data.results[0].formattedLocationFull,
+				searchRelativeTime: data.results[0].formattedRelativeTime,
+				searchResults: data.results
+			});
 		}
 	});
 });
