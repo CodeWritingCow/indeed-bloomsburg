a75f517a17b41985ea9c33b0ed9e447c3ad3e398
Gary Pang <gary.pang@gmail.com>
2016-06-24 23:28:55 -0400

    Add search form
    Add form to header.ejs. Add results_radius variable to config.js.

--- config.js
+++ config.js
@@ -5,5 +5,6 @@
 module.exports = {
 	'port': process.env.PORT || 8080, // set the port for our app
 	'publisher_id': process.env.PUBLISHER_ID, // set publisher ID for accessing Indeed API
-	'results_limit': 30 // set number of job search results
+	'results_limit': 30, // set number of job search results
+	'results_radius': 2
 };
--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/', function(req, res) {
 
 // berwick jobs 
 app.get('/berwick', function(req, res) {
-	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&l=berwick%2C+pa&sort=&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
+	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&l=berwick%2C+pa&sort=&radius=' + config.results_radius + '&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
 		if (!error && response.statusCode == 200) {
 			var data = JSON.parse(body);
 			res.render('pages/results', {
@@ -43,7 +43,7 @@ app.get('/berwick', function(req, res) {
 
 // bloomsburg jobs
 app.get('/bloomsburg', function(req, res) {
-	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&l=bloomsburg%2C+pa&sort=&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
+	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&l=bloomsburg%2C+pa&sort=&radius=' + config.results_radius + '&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
 		if (!error && response.statusCode == 200) {
 			var data = JSON.parse(body);
 			res.render('pages/results', {
@@ -56,7 +56,7 @@ app.get('/bloomsburg', function(req, res) {
 
 // danville jobs
 app.get('/danville', function(req, res) {
-	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&l=danville%2C+pa&sort=&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
+	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&l=danville%2C+pa&sort=&radius=' + config.results_radius + '&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
 		if (!error && response.statusCode == 200) {
 			var data = JSON.parse(body);
 			res.render('pages/results', {
