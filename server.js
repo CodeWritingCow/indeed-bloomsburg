92b2d493691df202bb032cfd11d214d8ea3d6b52
Gary Pang <gary.pang@gmail.com>
2016-07-01 01:29:52 -0400

    Create GET routes
    Make new routes for users to find jobs by employers. Make employers.ejs
    template.

--- server.js
+++ server.js
@@ -35,6 +35,11 @@ app.get('/towns', function(req, res) {
 	res.render('pages/towns');
 });
 
+// employers
+app.get('/employers', function(req, res) {
+	res.render('pages/employers');
+});
+
 // berwick jobs 
 app.get('/towns/berwick', function(req, res) {
 	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&l=berwick%2C+pa&sort=&radius=' + config.results_radius + '&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
@@ -155,9 +160,22 @@ app.post('/search', function(req, res) {
 	});
 });
 
+// alcoa jobs
+app.get('/employers/alcoa', function(req, res) {
+	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=alcoa&l=bloomsburg%2C+pa&sort=date&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var data = JSON.parse(body);
+			res.render('pages/results', {
+				searchLocation: data.location,
+				searchResults: data.results
+			});
+		}
+	});
+});
+
 // geisinger jobs
-app.get('/geisinger', function(req, res) {
-	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=geisinger&l=bloomsburg%2C+pa&sort=&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
+app.get('/employers/geisinger', function(req, res) {
+	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=geisinger&l=bloomsburg%2C+pa&sort=date&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
 		if (!error && response.statusCode == 200) {
 			var data = JSON.parse(body);
 			res.render('pages/results', {
@@ -169,8 +187,21 @@ app.get('/geisinger', function(req, res) {
 });
 
 // bloomsburg university jobs
-app.get('/bloomsburg-university', function(req, res) {
-	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=bloomsburg%20university&l=bloomsburg%2C+pa&sort=&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
+app.get('/employers/bloomsburg-university', function(req, res) {
+	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=bloomsburg%20university&l=bloomsburg%2C+pa&sort=date&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var data = JSON.parse(body);
+			res.render('pages/results', {
+				searchLocation: data.location,
+				searchResults: data.results
+			});
+		}
+	});
+});
+
+// bucknell university jobs
+app.get('/employers/bucknell-university', function(req, res) {
+	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=bucknell%20university&l=lewisburg%2C+pa&sort=date&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
 		if (!error && response.statusCode == 200) {
 			var data = JSON.parse(body);
 			res.render('pages/results', {
@@ -182,8 +213,60 @@ app.get('/bloomsburg-university', function(req, res) {
 });
 
 // berwick hospital center jobs
-app.get('/berwick-hospital-center', function(req, res) {
-	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=berwick%20hospital&l=berwick%2C+pa&sort=&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
+app.get('/employers/berwick-hospital-center', function(req, res) {
+	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=berwick%20hospital&l=berwick%2C+pa&sort=date&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var data = JSON.parse(body);
+			res.render('pages/results', {
+				searchLocation: data.location,
+				searchResults: data.results
+			});
+		}
+	});
+});
+
+// dollar general jobs
+app.get('/employers/dollar-general', function(req, res) {
+	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=dollar%20general&l=bloomsburg%2C+pa&sort=date&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var data = JSON.parse(body);
+			res.render('pages/results', {
+				searchLocation: data.location,
+				searchResults: data.results
+			});
+		}
+	});
+});
+
+// sheetz jobs
+app.get('/employers/sheetz', function(req, res) {
+	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=sheetz&l=bloomsburg%2C+pa&sort=date&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var data = JSON.parse(body);
+			res.render('pages/results', {
+				searchLocation: data.location,
+				searchResults: data.results
+			});
+		}
+	});
+});
+
+// wal-mart jobs
+app.get('/employers/wal-mart', function(req, res) {
+	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=walmart&l=bloomsburg%2C+pa&sort=date&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var data = JSON.parse(body);
+			res.render('pages/results', {
+				searchLocation: data.location,
+				searchResults: data.results
+			});
+		}
+	});
+});
+
+// weis markets jobs
+app.get('/employers/weis-markets', function(req, res) {
+	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=weis%20markets&l=bloomsburg%2C+pa&sort=date&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
 		if (!error && response.statusCode == 200) {
 			var data = JSON.parse(body);
 			res.render('pages/results', {
@@ -195,8 +278,8 @@ app.get('/berwick-hospital-center', function(req, res) {
 });
 
 // wise foods jobs
-app.get('/wise-foods', function(req, res) {
-	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=wise%20foods&l=berwick%2C+pa&sort=&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
+app.get('/employers/wise-foods', function(req, res) {
+	request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=wise%20foods&l=berwick%2C+pa&sort=date&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
 		if (!error && response.statusCode == 200) {
 			var data = JSON.parse(body);
 			res.render('pages/results', {
