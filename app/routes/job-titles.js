2fecfd08e039794ac3cc48d3eadaf4c383b8c13d
Gary Pang <gary.pang@gmail.com>
2016-07-02 23:26:51 -0400

    Add GET routes
    Create more routes for users to find jobs by title. Change job titles
    from plural to singular.

--- app/routes/job-titles.js
+++ app/routes/job-titles.js
@@ -10,8 +10,21 @@ module.exports = function(app, express) {
 		res.render('pages/job-titles');
 	});
 
-	// certified nursing assistants
-	jobTitleRouter.get('/certified-nursing-assistant', function(req, res) {
+	// cashier
+	jobTitleRouter.get('/cashier', function(req, res) {
+		request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=cashier&l=bloomsburg%2C+pa&sort=date&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var data = JSON.parse(body);
+				res.render('pages/results', {
+					searchLocation: data.location,
+					searchResults: data.results
+				});
+			}
+		});
+	});
+
+	// certified nursing assistant
+	jobTitleRouter.get('/certified-nursing-assistant-cna', function(req, res) {
 		request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=certified%20nursing%20assistant&l=bloomsburg%2C+pa&sort=date&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
 			if (!error && response.statusCode == 200) {
 				var data = JSON.parse(body);
@@ -23,7 +36,20 @@ module.exports = function(app, express) {
 		});
 	});
 
-	// receptionists
+	// licensed practicing nurse
+	jobTitleRouter.get('/licensed-practicing-nurse-lpn', function(req, res) {
+		request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=lpn&l=bloomsburg%2C+pa&sort=date&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var data = JSON.parse(body);
+				res.render('pages/results', {
+					searchLocation: data.location,
+					searchResults: data.results
+				});
+			}
+		});
+	});	
+
+	// receptionist
 	jobTitleRouter.get('/receptionist', function(req, res) {
 		request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=receptionist&l=bloomsburg%2C+pa&sort=date&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
 			if (!error && response.statusCode == 200) {
@@ -36,8 +62,8 @@ module.exports = function(app, express) {
 		});
 	});
 
-	// registered nurses
-	jobTitleRouter.get('/registered-nurse', function(req, res) {
+	// registered nurse
+	jobTitleRouter.get('/registered-nurse-rn', function(req, res) {
 		request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=registered%20nurse&l=bloomsburg%2C+pa&sort=date&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
 			if (!error && response.statusCode == 200) {
 				var data = JSON.parse(body);
@@ -49,7 +75,7 @@ module.exports = function(app, express) {
 		});
 	});
 
-	// cooks
+	// cook
 	jobTitleRouter.get('/cook', function(req, res) {
 		request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=cook&l=bloomsburg%2C+pa&sort=date&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
 			if (!error && response.statusCode == 200) {
@@ -62,9 +88,22 @@ module.exports = function(app, express) {
 		});
 	});	
 
-	// sales associates
+	// sales associate
 	jobTitleRouter.get('/sales-associate', function(req, res) {
 		request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=sales%20associate&l=bloomsburg%2C+pa&sort=date&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var data = JSON.parse(body);
+				res.render('pages/results', {
+					searchLocation: data.location,
+					searchResults: data.results
+				});
+			}
+		});
+	});
+
+	// therapist
+	jobTitleRouter.get('/therapist', function(req, res) {
+		request('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&q=therapist&l=bloomsburg%2C+pa&sort=date&radius=&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function(error, response, body) {
 			if (!error && response.statusCode == 200) {
 				var data = JSON.parse(body);
 				res.render('pages/results', {
