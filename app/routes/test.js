d2401f506541530ea3a272f5b201db22c7c9c660
Gary Pang <gary.pang@gmail.com>
2016-07-07 02:42:48 -0400

    Update test.js
    App saves data returned by testHandler( ) to global var JobUrl. Data is
    now available on global scope.

--- app/routes/test.js
+++ app/routes/test.js
@@ -5,17 +5,23 @@ var bodyParser = require('body-parser'),
 	config = require('../../config');
 
 module.exports = function(app, express) {
+
+	// global variable for capturing data from testHandler(url)
+	var jobUrl;
 	
 	var testRouter = express.Router();
 
 	// test function
-	function testHandler(jobUrl) {
+	function testHandler(url) {
 
-		request(jobUrl, function(error, response, body) { // this works. request() makes HTTP calls and gets response.
+		request(url, function(error, response, body) { // this works. request() makes HTTP calls and gets response.
 			if (!error && response.statusCode == 200) {
 				var data = JSON.parse(body); // this works. Parses JSON response from request() and saves it as var data.
-				console.log(data.location); // this works.
+				//console.log(data.location); // this works.
 				// console.log(jobUrl); // this works.
+				// jobUrl = "I'm at " + data.location;
+				// jobUrl = url; // this works
+				return;
 			}
 		});
 	}
@@ -35,9 +41,12 @@ module.exports = function(app, express) {
 			res.end();
 		});*/
 		testHandler('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&l=berwick%2C+pa&sort=&radius=' + config.results_radius + '&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2'); // this works. passes url to testHandler().
-		console.log(data); // this doesn't work. returns "undefines"
-		
-		res.end();
+		console.log(jobUrl); // this now works. returns results from Indeed job search.
+		// res.send(jobUrl); // this works
+		res.render('pages/results', {
+			searchLocation: jobUrl.location,
+			searchResults: jobUrl.results
+		});
 	});
 
 	return testRouter;
