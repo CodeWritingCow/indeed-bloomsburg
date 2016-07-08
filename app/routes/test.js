56d1f7a5c30a8e42f8a537fa2e609dd47f5ea70b
Gary Pang <gary.pang@gmail.com>
2016-07-08 02:19:43 -0400

    Create test.ejs
    Make view template for test route. Add GET routes to test.js (copied
    them from towns.js) to test them with urlHandler( ).
    TODO: Fix urlHander( ). When users visit a route for the first time,
    they get "TypeError: Cannot read property 'location' of undefined."
    Because jobData is still undefined. If they refresh their browser, then
    website does return results.

--- app/routes/test.js
+++ app/routes/test.js
@@ -1,41 +1,80 @@
-// this is a test route for testing middlewares and new site features
- 
 var bodyParser = require('body-parser'),
 	request = require('request'),
 	config = require('../../config');
 
 module.exports = function(app, express) {
 
-	// global variable for capturing data from testHandler(url)
-	var jobData;
-	
 	var testRouter = express.Router();
-
-	// test function
-	function testHandler(url) {
-
+	
+	// makes HTTP call and returns results
+	function urlHandler(url) {
 		request(url, function(error, response, body) {
 			if (!error && response.statusCode == 200) {
 				var data = JSON.parse(body);
 				jobData = data; // saves job search results to global scope
-				return;
+				return jobData;
 			}
 		});
-	}
+	}	
 
-	// test middleware
-	testRouter.use('/', function(req, res, next) {
-		console.log("Job seeker has arrived on our test route!");
-		next();
-	});	
+	// global variable for capturing data from urlHandler(url)
 
-	// test route 
+	// towns
 	testRouter.get('/', function(req, res) {
-		testHandler('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&l=berwick%2C+pa&sort=&radius=' + config.results_radius + '&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2');
+	});
+
+	// berwick jobs 
+	testRouter.get('/berwick', function(req, res) {
+		urlHandler('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&l=berwick%2C+pa&sort=&radius=' + config.results_radius + '&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2');
 		res.render('pages/results', {
 			searchLocation: jobData.location,
 			searchResults: jobData.results
 		});
+	});
+
+	// bloomsburg jobs
+	testRouter.get('/bloomsburg', function(req, res) {
+		urlHandler('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&l=bloomsburg%2C+pa&sort=&radius=' + config.results_radius + '&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2');
+		res.render('pages/results', {
+			searchLocation: jobData.location,
+			searchResults: jobData.results
+			});
+	});
+
+	// danville jobs
+	testRouter.get('/danville', function(req, res) {
+		urlHandler('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&l=danville%2C+pa&sort=&radius=' + config.results_radius + '&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2');
+		res.render('pages/results', {
+			searchLocation: jobData.location,
+			searchResults: jobData.results
+			});
+	});
+
+	// lewisburg jobs
+	testRouter.get('/lewisburg', function(req, res) {
+		urlHandler('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&l=lewisburg%2C+pa&sort=date&radius=' + config.results_radius + '&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2');
+		res.render('pages/results', {
+			searchLocation: jobData.location,
+			searchResults: jobData.results
+			});
+	});
+
+	// milton jobs
+	testRouter.get('/milton', function(req, res) {
+		urlHandler('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&l=milton%2C+pa&sort=date&radius=' + config.results_radius + '&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2');
+		res.render('pages/results', {
+			searchLocation: jobData.location,
+			searchResults: jobData.results
+			});
+	});
+
+	// sunbury jobs
+	testRouter.get('/sunbury', function(req, res) {
+		urlHandler('http://api.indeed.com/ads/apisearch?publisher=' + config.publisher_id + '&format=json&l=sunbury%2C+pa&sort=date&radius=' + config.results_radius + '&st=&jt=&start=&limit=' + config.results_limit + '&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2');
+		res.render('pages/results', {
+			searchLocation: jobData.location,
+			searchResults: jobData.results
+			});
 	});
 
 	return testRouter;
